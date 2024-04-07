0c45e65187562ea306bc1374943ff7863e491c26
aay7ush <aay7ush@gmail.com>
2024-04-07 16:44:07 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,38 @@
-const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+import { useContext } from "react"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { AuthContext } from "./context/AuthContext"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+function App() {
+	const { currentUser } = useContext(AuthContext)
+
+	const ProtectedRoute = ({ children }) => {
+		if (!currentUser) {
+			return <Navigate to="/login" />
+		}
+		return children
+	}
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/">
+					<Route
+						index
+						element={
+							<ProtectedRoute>
+								<Home />
+							</ProtectedRoute>
+						}
+					/>
+					<Route path="login" element={<Login />} />
+					<Route path="register" element={<Register />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
+
 export default App
